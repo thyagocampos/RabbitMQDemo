270ac0dd13d5c94453e63bb6bf68abcbacbfc512
Thyago <thyagosic@gmail.com>
2023-09-10 15:33:36 -0300

    Adicionar arquivos de projeto.

---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Criando a conexão para o servidor
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver1 App";
+IConnection cnn = factory.CreateConnection();
+IModel channel = cnn.CreateModel();
+
+//Criando o canal
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+//prefetchSize  -> não importa o tamanho da mensagem
+//prefetchCount -> quantas mensagens queremos que seja enviada a você a cada vez
+//o último parãmetro -> indica se iremos aplicar essas configs apenas para essa instância ou todo sistema
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+
+    var body = args.Body.ToArray();
+    
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message Received: {message}");
+
+    //Setamos a mensagem como entregue
+    channel.BasicAck(args.DeliveryTag, false);
+    
+    //Se ouver lagum problema podemos setar a mensagem com a taga de delivery false 
+};
+
+string consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+cnn.Close();
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Criando a conexão para o servidor
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+IConnection cnn = factory.CreateConnection();
+IModel channel = cnn.CreateModel();
+
+//Criando o canal
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+//Criando e enviando a mensagem
+for (int i = 0; i < 60; i++) {        
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message number {i}");
+
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+    Console.WriteLine($"Sending message {i}");
+
+    Thread.Sleep(1000);
+}
+
+channel.Close();
+cnn.Close();
